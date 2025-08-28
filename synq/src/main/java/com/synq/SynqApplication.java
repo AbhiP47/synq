5fe2be8a07b55ea73e8f8ede652d476214da4a06
AbhiP47 <abhinavjaipanwar@gmail.com>
2025-08-28 14:43:35 +0530

    initial commit

---
+++ synq/src/main/java/com/synq/SynqApplication.java
@@ -0,0 +1,13 @@
+package com.synq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynqApplication.class, args);
+	}
+
+}
---
+++ synq/src/test/java/com/synq/synq/SynqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synq.synq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
