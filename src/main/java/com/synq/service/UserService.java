e165c6e80a6cb2e8d2b68d3fbe4ea62d1258235b
AbhiP47 <abhinavjaipanwar@gmail.com>
2025-09-05 09:39:47 +0530

    user service and its implementation

--- src/main/java/com/synq/service/UserService.java
+++ src/main/java/com/synq/service/UserService.java
@@ -1,4 +1,16 @@
 package com.synq.service;
 
-public class UserService {
+import com.synq.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+    User saveUser(User user);
+    Optional<User> getUserById(String id);
+    Optional<User> updateUser(User user);
+    void deleteUser(String id);
+    boolean isUserExist(String userId);
+    boolean isUserExistByEmail(String email);
+    List<User> getAllUsers();
 }
---
+++ src/main/java/com/synq/service/UserServiceImpl.java
@@ -0,0 +1,73 @@
+package com.synq.service;
+
+import com.synq.helpers.ResourceNotFoundException;
+import com.synq.entity.User;
+import com.synq.repository.UserRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class UserServiceImpl implements  UserService{
+    @Autowired
+    private UserRepo userRepo;
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public User saveUser(User user) {
+        String userId = UUID.randomUUID().toString();
+        user.setId(userId);
+        return userRepo.save(user);
+    }
+
+    @Override
+    public Optional<User> getUserById(String id) {
+        return userRepo.findById(id);
+    }
+
+    @Override
+    public Optional<User> updateUser(User user) {
+        User userOld = userRepo.findById(user.getId()).orElseThrow( ()-> new ResourceNotFoundException("User not found"));
+        userOld.setName(user.getName());
+        userOld.setEmail(user.getEmail());
+        userOld.setPassword(user.getPassword());
+        userOld.setAbout(user.getAbout());
+        userOld.setPhoneNumber(user.getPhoneNumber());
+        userOld.setProfilePic(user.getProfilePic());
+        userOld.setEnabled(user.isEnabled());
+        userOld.setEmailVerified(user.isEmailVerified());
+        userOld.setPhoneVerified(user.isPhoneVerified());
+        userOld.setProvider(user.getProvider());
+        User saved = userRepo.save(userOld);
+        return Optional.ofNullable(saved);
+    }
+
+    @Override
+    public void deleteUser(String id) {
+        User user = userRepo.findById(id).orElseThrow(()-> new ResourceNotFoundException("User not found"));
+        userRepo.delete(user);
+    }
+
+    @Override
+    public boolean isUserExist(String userId) {
+        User user = userRepo.findById(userId).orElse(null);
+        return user != null ? true : false;
+    }
+
+    @Override
+    public boolean isUserExistByEmail(String email) {
+        User user = userRepo.findByEmail(email).orElse(null);
+        return user != null ? true:false;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepo.findAll();
+    }
+}
