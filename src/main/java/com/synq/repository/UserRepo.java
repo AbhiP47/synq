cef8cf45fba453c387ccba40ba026daa7c5058ed
AbhiP47 <abhinavjaipanwar@gmail.com>
2025-09-10 22:23:54 +0530

    user dashboard

---
+++ src/main/java/com/synq/repository/UserRepo.java
@@ -0,0 +1,10 @@
+package com.synq.repository;
+
+import com.synq.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepo extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
+}
